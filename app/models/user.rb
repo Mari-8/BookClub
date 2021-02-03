c1700036d49a9faa5dbd19e17a9cc8573b583d65
Mari-8 <ryanaltimari8@gmail.com>
2021-02-03 12:24:09 -0600

    added method to create user from omniauth, vadiladion for email presence and uniqeness, and has_secure_password macro

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :email, presence: true, uniqueness: true
+
+
+    def self.create_from_omniauth(auth)
+        user = User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u| 
+            u.username = auth['info']['name'] 
+            u.email = auth['info']['name'] 
+            u.password = SecureRandom.hex(12)
+        end 
+    end 
+end
