27b09282be4d5c3ef9de6ae9b783c09e2feae76c
Mari-8 <ryanaltimari8@gmail.com>
2021-02-03 12:23:09 -0600

    created users controller, new, create, and show actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+
+
+    def new 
+        @user = User.new 
+    end 
+
+    def create 
+        @user = User.new(user_params)
+
+        if @user.save
+            flash[:notice] = "Account created successfully!"
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+          else
+            flash.now.alert = "There was a problem creating the account. Please make sure you are using a valid email and password and try again."
+            render :new
+          end
+    end 
+
+    def welcome 
+
+    end 
+
+    def show 
+        @user = User.find_by_id(params[:id])
+    end 
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:username, :password, :password_confirmation, :email, :bio)
+    end 
+
+end 
