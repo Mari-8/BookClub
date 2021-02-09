0cd21e7af076d5e6cdc9a1eb569c1868bff0bce6
Mari-8 <ryanaltimari8@gmail.com>
2021-02-09 17:22:20 -0600

    added route protection on edit, update, destroy, show actions. require login and users shelf method

--- app/controllers/bookshelves_controller.rb
+++ app/controllers/bookshelves_controller.rb
@@ -1,19 +1,21 @@
 class BookshelvesController < ApplicationController
 
+    before_action :require_login
+
     def new 
         @bookshelf = Bookshelf.new 
         @books = current_user.books
     end 
 
     def create 
-        bookshelf = Bookshelf.create(shelf_name: params[:bookshelf][:shelf_name])
+        bookshelf = Bookshelf.create(bookshelf_params)
         bookshelf.user_id = current_user.id
 
         if bookshelf.save 
             flash[:notice] = "Successfully created shelf" 
             redirect_to bookshelf_path(bookshelf)
         else 
-            flash[:alert] = bookshelf.errors.full_messages.to_sentence
+            flash[:alert] = "#{bookshelf.errors.full_messages.to_s}"
             redirect_to new_bookshelf_path 
         end 
        
@@ -21,20 +23,61 @@ class BookshelvesController < ApplicationController
 
     def show 
         @bookshelf = Bookshelf.find_by_id(params[:id])
+
+        if !users_shelf(@bookshelf) 
+            flash[:alert] = "Cannot view another users bookshelf" 
+            redirect_to user_path(current_user) 
+        end 
     end 
     
     def index 
-        @bookshelves = Bookshelf.all
+        @bookshelves = current_user.bookshelves 
     end 
 
 
     def edit 
+        @bookshelf = Bookshelf.find(params[:id])
 
+        if !users_shelf(@bookshelf) 
+            flash[:alert] = "cannot edit another users shelf" 
+            redirect_to user_path(current_user) 
+        end 
+    end 
+
+    def update 
+        @bookshelf = Bookshelf.find(params[:id])
+        
+        if users_shelf(@bookshelf) && @bookshelf.update(bookshelf_params)
+            redirect_to bookshelf_path(@bookshelf) 
+        else 
+            flash[:alert] = "#{@bookshelf.errors.full_messages}"
+            redirect_to user_path(current_user)
+        end 
     end 
 
     def destroy 
+        shelf = Bookshelf.find(params[:id])
 
+        if users_shelf(shelf) 
+            shelf.destroy 
+            redirect_to bookshelves_path 
+        else 
+            flash[:alert] = "Cannot delete another users shelf"
+        end 
     end 
 
+    private 
+
+    def bookshelf_params 
+        params.require(:bookshelf).permit(:shelf_name)
+    end 
+
+    def require_login 
+        return head(:forbidden) unless session.include? :user_id
+    end 
+
+    def users_shelf(bookshelf) 
+        bookshelf.user_id == current_user.id 
+    end 
 
 end
