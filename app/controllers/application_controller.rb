b137b9107c04f1aa334e2dc617274861a0c1408b
Mari-8 <ryanaltimari8@gmail.com>
2021-02-09 17:19:53 -0600

    added users_book & review methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,24 @@
 class ApplicationController < ActionController::Base
-
+    helper_method :current_user
+    helper_method :logged_in?
 
     def current_user
-        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+        if session[:user_id]
+            @current_user ||= User.find(session[:user_id])
+        else 
+            nil 
+        end 
     end
 
     def logged_in?    
         !current_user.nil?
     end
+
+    def users_book(book)
+        book.user_id == current_user.id
+    end 
+
+    def users_review(review)
+        review.user_id == current_user.id
+    end 
 end
