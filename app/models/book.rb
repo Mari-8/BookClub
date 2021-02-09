8dc20f68abf13fb1b1f169aaf670a9ad682c5ab6
Mari-8 <ryanaltimari8@gmail.com>
2021-02-09 17:55:34 -0600

    added validation for shelf_id, added favorite_books class method

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,19 @@
 class Book < ApplicationRecord
-
     belongs_to :user
     belongs_to :bookshelf
+    has_many :reviews 
+
+    validates :bookshelf_id, presence: true 
+    
+
     def create_from_google_books(user_input)
         results = GoogleBooks.search(user_input)
         book = results.first 
-        book = Book.create(title: book.title, author_name: book.authors, purchase_info: book.sale_info['buyLink'], image_url: book.image_link, description: book.description)
+        book = Book.create(title: book.title, author_name: book.authors, image_url: book.image_link, description: book.description)
+    end 
+
+    def self.favorite_books(user)
+        user.books.where(favorite: true)
     end 
 end
   
