6314ec1d03f441864ffcdbcf632e835ae4863f3a
Mari-8 <ryanaltimari8@gmail.com>
2021-02-09 19:47:58 -0600

    added discussion and coment relationships, as well as join table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :books 
-    has_many :bookshelves 
+    has_many :bookshelves
+    has_many :reviews
+    has_many :discussions 
+    has_many :comments
+    has_many :entered_discussions, through: :comments, source: :discussion
+   
 
     validates :email, presence: true, uniqueness: true
-
+    validates :password, length: { in: 6..20 }
+    validates :bio, length: { maximum: 350 }
 
     def self.create_from_omniauth(auth)
         user = User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u| 
@@ -13,4 +19,6 @@ class User < ApplicationRecord
             u.password = SecureRandom.hex(12)
         end 
     end 
+
+  
 end
